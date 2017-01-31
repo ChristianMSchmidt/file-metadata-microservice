8d752c86970eff50e2fd67f45302a602c9f8a3cd
Christian Schmidt <chris.kueps@gmail.com>
2017-01-31 18:53:47 +0000

    added cors-module for beta.freeCodeCamp

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 "use strict";
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const multer = require("multer");
 const upload = multer(); //From Multer Doc: In case you omit the options object, the files will be kept in memory and never written to disk.
 
+app.use(cors());
 app.use("/", express.static("./public"));
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req,res,next) => {
